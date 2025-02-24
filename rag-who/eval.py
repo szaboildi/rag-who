0a93193c1dd864b15223770c81fc4aaa2d3b9bc7
szaboildi <szabo.ildikomse@gmail.com>
2025-02-24 15:43:16 +0100

    Clarify in the function names that recall and mrr are evaluated on a sentence-level

--- rag-who/eval.py
+++ rag-who/eval.py
@@ -2,39 +2,45 @@ import os
 import json
 from retreival import setup_vector_db, query_vector_db_list
 
-def eval_recall(results:list[dict[str,str]],
-                correct_answers:list[dict[str,str]]):
+def eval_recall_sentence(
+    results:list[dict[str,str]], correct_answers_sent:list[dict[str,str]]):
     recalls = []
     not_found_answers = []
 
     for i in range(len(results)):
         retreived_passages = [a["text"] for a in results[i]["answers"]]
         not_found_answers_i = [
-            corr_a for corr_a in correct_answers[i]["answers"]
+            corr_a for corr_a in correct_answers_sent[i]["answers"]
             if not any([corr_a in r for r in retreived_passages])]
 
         recall = sum([
             any([corr_a in r
                  for r in retreived_passages])
-            for corr_a in correct_answers[i]["answers"]]) / \
-                len(correct_answers[i]["answers"])
+            for corr_a in correct_answers_sent[i]["answers"]]) / \
+                len(correct_answers_sent[i]["answers"])
 
         recalls.append(recall)
         not_found_answers.append(not_found_answers_i)
 
     return recalls, not_found_answers
 
-def eval_mrr(
-    results:list[dict[str,str]], correct_answers:list[dict[str,str]]):
+
+        recalls.append(recall)
+        not_found_answers.append(not_found_answers_i)
+
+    return recalls, not_found_answers
+
+def eval_mrr_sentence(
+    results:list[dict[str,str]], correct_answers_sent:list[dict[str,str]]):
     mrr = []
-    for i in range(len(correct_answers)):
+    for i in range(len(correct_answers_sent)):
         retreived_passages = [a["text"] for a in results[i]["answers"]]
         for j in range(len(retreived_passages)):
             # Iterating through the retrieved passages, find the first one
             # that's relevant, and add it's RR to the the accumulator list
             if any([
                 corr_a in retreived_passages[j]
-                for corr_a in correct_answers[i]["answers"]]):
+                for corr_a in correct_answers_sent[i]["answers"]]):
                 mrr.append(1/(j+1))
                 break
         # If none of the retreived passages were relevant
