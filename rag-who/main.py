04d237196136ce163314cf319419aa5d8be76915
szaboildi <szabo.ildikomse@gmail.com>
2025-02-28 13:36:54 +0100

    LLM generation

--- rag-who/main.py
+++ rag-who/main.py
@@ -1,9 +1,13 @@
 import os
 import json
 import argparse
+from openai import OpenAI
+import pandas as pd
+import time
 
-from retrieval import setup_vector_db, query_vector_db_list
+from retrieval import setup_vector_db, query_vector_db_list, query_vector_db_once
 from eval import eval_recall_sentence, eval_recall_passage, eval_mrr_sentence, ndcg_scorer_manual
+from generation import create_qa_string, api_call #, QuestionAnswering
 
 
 try:
@@ -11,7 +15,7 @@ try:
 except ModuleNotFoundError:
     import tomli as tomllib
 
-def retrieve_and_eval(config_name="default"):
+def retrieve_and_eval(config_name:str="default"):
     with open(os.path.join(config[config_name]["input_folder_qa"],
                            config[config_name]["sample_qa_file"])) as f:
         correct_answers = json.load(f)
@@ -31,7 +35,7 @@ def retrieve_and_eval(config_name="default"):
     results = query_vector_db_list(
         client, encoder, input_queries,
         collection_name = config[config_name]["collection_name"],
-        k=config[config_name]["k"])
+        k=config[config_name]["retrieve_k"])
     print("Queries completed")
 
     with open(os.path.join(
@@ -62,6 +66,65 @@ def retrieve_and_eval(config_name="default"):
 
 
 
+def rag_setup(config_name:str="default", api_key_variable:str="OPENAI_API_KEY"):
+    vector_db_client, encoder = setup_vector_db(
+        encoder_name=config[config_name]["encoder_name"],
+        client_source=config[config_name]["client_source"],
+        input_folder=config[config_name]["input_text_folder"],
+        chunk_length=config[config_name]["chunk_length"],
+        chunk_overlap_words=config[config_name]["chunk_overlap"],
+        collection_name=config[config_name]["collection_name"],
+        dist_name=config[config_name]["distance_type"],
+        input_folder_qa=config[config_name]["input_folder_qa"],
+        relevance_score_file_prefix=config[config_name]["relevance_score_file_prefix"],
+        sample_qa_file=config[config_name]["sample_qa_file"])
+
+    api_client = OpenAI(api_key=os.environ.get(api_key_variable))
+
+    print("RAG setup complete")
+    return vector_db_client, encoder, api_client
+
+
+def rag_query_once(
+    query:str, vector_db, encoder, api_client, config_name:str="default"):
+    retrieved_doc_dict = query_vector_db_once(
+        vector_db, encoder, query,
+        collection_name = config[config_name]["collection_name"],
+        k=config[config_name]["retrieve_k"])
+    print("Documents retrieved")
+
+    user_prompt = create_qa_string(query, retrieved_doc_dict["answers"])
+
+    response = api_call(
+        client=api_client, user_prompt=user_prompt,
+        system_propmt_path=config[config_name]["llm_system_prompt_path"],
+        model=config[config_name]["llm_model"],
+        temperature=config[config_name]["llm_temperature"])
+
+    return query, response
+
+
+def rag_query_list(
+    queries:list[str], vector_db, encoder, api_client, config_name:str="default"):
+    responses = [rag_query_once(
+        q, vector_db, encoder, api_client, config_name) for q in queries]
+
+    return queries, responses
+
+
+def export_qa_lists(queries:list[str], responses:list[str],
+                    model:str, temperature:float, export_folder:str):
+    qa_df = pd.DataFrame({"Query": queries, "Response": responses})
+
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    qa_df.to_csv(
+        os.path.join(
+            export_folder, f"QA_{model}_temp{temperature}_{timestamp}.csv"),
+        index=False)
+
+    return
+
+
 if __name__ == "__main__":
     with open("rag-who.toml", mode="rb") as fp:
         config = tomllib.load(fp)
@@ -70,5 +133,17 @@ if __name__ == "__main__":
     parser.add_argument("--config_name", nargs='?', default="default")
     args=parser.parse_args()
 
-    print(args.config_name)
-    retrieve_and_eval(config_name=args.config_name)
+    # print(args.config_name)
+    # retrieve_and_eval(config_name=args.config_name)
+    vector_db_client, encoder, api_client = rag_setup()
+    query, response = rag_query_once(
+        "How long do rabbits live?",
+        vector_db_client, encoder, api_client)
+    print("\n", query, response, sep="\n")
+
+    # print("#########################################")
+
+    # response = rag_query_once(
+    #     "How many deaths does alcoholism cause a year in the European Region?",
+    #     vector_db_client, encoder, api_client)
+    # print(response)
